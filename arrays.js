3774f8d30c78cc778ac50f3624e4b5f55d216a18
Vishwanath C <meetvishwa123@rediffmail.com>
2023-02-16 18:00:32 +0530

    Update arrays.js

--- arrays.js
+++ arrays.js
@@ -19,3 +19,24 @@ function evenNumbers(array) {
 
 2) Write a function that takes in an array of numbers and returns the sum of all the numbers in the array. For example, if the input array is [1, 2, 3, 4, 5], the function should return 15. If the input array is [10, -2, 5.5, 3], the function should return 16.5.
 
+function evenNumbers(array) {
+  // create an empty array to store even integers
+  let sum=0;
+
+  // loop through the input array
+  for (let i = 0; i < array.length; i++) {
+    // check if the current element is even
+    console.log(array[i]);
+    sum = sum+ array[i];
+ 
+  }
+//console.log(sum);
+  return sum;
+}
+
+const inputArray = [1, 2, 3, 4, 5];
+const outputArray = evenNumbers(inputArray);
+console.log(outputArray); // should log [2, 4, 6]
+//------------------------------------------------------------------------------------------------------------------------------
+
+3)
